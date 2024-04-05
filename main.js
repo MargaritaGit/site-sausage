3863f5d65caa8a00c030ee60f6969cef2835537f
MargaritaGit <samplefinessa@gmail.com>
2024-04-05 17:15:29 +0300

    Initial commit: header with image

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict'
+// Цикл for
+{
+    let i = 0;
+    if (i < 3) {
+        console.log(i);
+    }
+}
+// i++;
+
+{
+    let i = 1;
+    if (i < 3) {
+        console.log(i);
+    }
+}
+// i++;
+
+{
+    let i = 2;
+    if (i < 3) {
+        console.log(i);
+    }
+}
