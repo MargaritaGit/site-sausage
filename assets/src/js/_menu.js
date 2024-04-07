ec03858aaf0cff0d5b7a70ecfbbf9c8846d361d8
Igor M. <samplefines@gmail.com>
2024-04-07 15:37:58 +0300

    mobile menu

---
+++ assets/src/js/_menu.js
@@ -0,0 +1,45 @@
+export function toggleMobileMenu() {
+    const menu = document.querySelector('.nav');
+    const menuIcon = document.querySelector('.top-header__icon-menu');
+    const menuCloseIcon = menu.querySelector('.nav_mob__icon-close-menu');
+    const logo = menu.querySelector('.logo a');
+
+    menuIcon.onclick = () => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+        menu.classList.toggle('nav_mob');
+    }
+
+    // отключаем моб. меню при ресайзе до десктопа
+    window.addEventListener('resize', () => {
+        if (window.innerWidth >= 1024) {
+            menu.classList.remove('nav_mob');
+        }
+    })
+
+    // запрещаем прокрутку страницы при открытии меню
+    // на iPhone всё равно можно прокрутить страницу
+    document.body.style.overflow = 'hidden';
+    document.body.style.height = '100vh';
+
+
+    // закрытие меню
+    menu.onclick = (e) => {
+        // console.log(e.target);
+        if (window.innerWidth < 1024 && (
+            e.target === menu ||
+            e.target === menuCloseIcon ||
+            e.target === logo)) {
+            menu.classList.toggle('nav_mob');
+            // возобновляем возможность прокрутки страницы при закрытии меню
+            document.body.style.overflow = 'visible';
+        }
+    }
+
+    // устанавливаем задерку анимации (чтобы не писать вручную через CSS)
+    const menuItems = document.querySelectorAll('.nav-list__item');
+    menuItems.forEach((menuItem, i) => {
+        // console.log(menuItem);
+        // console.log((0 + (i / 15)) + 's');
+        menuItem.style.animationDelay = (0 + (i / 12)) + 's';
+    });
+}
--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -1,8 +1,11 @@
 import { flexOrderSlider } from "./_sliderHeader";
+import { toggleMobileMenu } from "./_menu";
 
-// flexOrderSlider('.slider', '2s ease-in-out');
-flexOrderSlider('.slider', '2s ease-in-out', 6000);
+flexOrderSlider('.slider', '2s ease-in-out');
+// flexOrderSlider('.slider', '2s ease-in-out', 6000);
 
+
+toggleMobileMenu();
 
 
 
