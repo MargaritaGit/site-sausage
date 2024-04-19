e2d97d833abf056ad5fa81d69065bf3598d12a82
Igor M. <samplefines@gmail.com>
2024-04-19 22:45:27 +0300

    _linksJustScroll.js linksJustScroll('.nav', '.logo_mob') - for links inside '.nav', '.logo_mob'

--- assets/src/js/_button_up.js
+++ assets/src/js/_button_up.js
@@ -1,8 +1,7 @@
 export function buttonUp(scrolledCoeff = 1.1) {
 
-
     const btnUp = document.getElementById('btn_up');
-    console.log(btnUp);
+    // console.log(btnUp);
 
     if (window.scrollY > (scrolledCoeff * window.innerHeight)) {
         btnUp.style.visibility = 'visible';
@@ -10,14 +9,11 @@ export function buttonUp(scrolledCoeff = 1.1) {
     }
 
     document.onscroll = (event) => {
-        // console.log(event);
-        // console.log(window.scrollY);
+
         if (window.scrollY < (scrolledCoeff * window.innerHeight)) {
             btnUp.style.visibility = 'hidden';
             btnUp.style.opacity = 0;
         } else {
-            // console.log(window.innerHeight);
-            // console.log(window.scrollY);
             btnUp.style.visibility = 'visible';
             btnUp.style.opacity = 1;
         }
