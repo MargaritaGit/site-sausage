927ac64e1ff36c831899649e0ec9333351a31a77
Igor M. <samplefines@gmail.com>
2024-04-23 16:43:49 +0300

    fixed bug for devices without  lvh support - CSS var --screenHeight set by JS

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -4,6 +4,13 @@ import { buttonUp } from "./_button_up";
 import { linksOnPageJustScroll } from "./_linksJustScroll";
 import { productsFlyInEffect } from "./_products_fx"
 
+// задаём высоту псевдоэлемента с фикс.фоном для устойств, не поддерживающих еденицы измерения lvh - Largest Viewport Height
+
+// document.querySelector(':root').style = `--screenHeight: 100%`
+document.querySelector(':root').style = `--screenHeight: ${window.outerHeight}px`
+
+
+
 // flexOrderSlider('.slider', '2s ease-in-out');
 flexOrderSlider('.slider', '2s ease-in-out', 8000);
 
@@ -15,6 +22,8 @@ linksOnPageJustScroll();
 
 productsFlyInEffect('transform 0.65s ease-in-out, opacity 0.65s ease-in-out');
 // productsFlyInEffect('0.7s ease-out');
+
+
 
 
 
