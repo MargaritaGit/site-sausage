48918a6d29b5ceb72d5ce80f6e571b6c7aa1f74c
MargaritaGit <samplefinessa@gmail.com>
2024-04-29 20:13:02 +0300

    Modal with open/close animation

--- assets/src/js/modal.js
+++ assets/src/js/modal.js
@@ -3,23 +3,23 @@
 const coopArr = document.querySelectorAll('.coop');
 const modal = document.getElementById('modal');
 const closeModalBtn = modal.querySelector('.modal__close');
-// const content = modal.querySelector('.modal__content')
+const modalContent = modal.querySelector('.modal__content');
 
 
 for (const coop of coopArr) {
     coop.onclick = (e) => {
-        modal.style.display = "block";
+        modal.style.display = "flex";
         modal.addEventListener('click', closeModal);
 
     }
 }
 
-closeModalBtn.addEventListener('click', (e) => {
-    console.log(e.target);
-    console.log(e.currentTarget);
-    modal.style.display = "none";
+// closeModalBtn.addEventListener('click', (e) => {
+//     console.log(e.target);
+//     console.log(e.currentTarget);
+//     modal.style.display = "none";
 
-});
+// });
 
 // modal.addEventListener('click', (e) => {
 //     console.log(e.currentTarget);
@@ -36,10 +36,33 @@ closeModalBtn.addEventListener('click', (e) => {
 function closeModal(e) {
     console.log(e.target);
     console.log(e.currentTarget);
-    if (e.target === e.currentTarget) {
-        modal.style.display = "none";
-        modal.removeEventListener('click', closeModal);
+
+    if (e.target === e.currentTarget || e.target === closeModalBtn) {
+        modalContent.classList.remove('scale-up-center');
+        modalContent.classList.add('scale-down-center');
+
+        // setTimeout(() => {
+        //     // console.log('я внутри колбека settimeout');
+
+        //     modal.style.display = "none";
+        //     modalContent.classList.remove('scale-down-center');
+        //     modalContent.classList.add('scale-up-center');
+
+        //     modal.removeEventListener('click', closeModal);
+        // }, 400);
+
+
+        modalContent.addEventListener('animationend', () => {
+            // console.log('я внутри колбека animationend');
+
+            modal.style.display = "none";
+            modalContent.classList.remove('scale-down-center');
+            modalContent.classList.add('scale-up-center');
+
+            modal.removeEventListener('click', closeModal);
+        }, { once: true });
     }
+
 }
 
 
