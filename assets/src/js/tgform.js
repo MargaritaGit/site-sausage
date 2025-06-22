9a91f10bbe642ab216d425ba9874e93950023812
Igor M. <samplefines@gmail.com>
2025-06-22 12:59:33 +0300

    Added user-confirm link for free HTTPS-proxy in form

--- assets/src/js/tgform.js
+++ assets/src/js/tgform.js
@@ -16,6 +16,7 @@ async function formSubmitSafeBackEnd(event) {
     return;
   }
 
+  // php скрипт будет приминать массив строк - имя и телефон
   const formData = [];
 
   formData.push(userName);
@@ -32,30 +33,47 @@ async function formSubmitSafeBackEnd(event) {
   const BACKEND_URL =
     'http://u995982r.beget.tech/for-site-sausage-token/senderToTgBot.php';
 
-  const response = await fetch(proxyURL + BACKEND_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded', // OK fo free http backend
-      // 'Content-Type': 'application/json', // may Cause Error: Access to fetch at '....' from origin '....' has been blocked by CORS policy: Response to preflight request doesn't pass access control  on https://cors-anywhere.herokuapp.com/'
+  try {
+    const response = await fetch(proxyURL + BACKEND_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded', // OK fo free http backend
+        // 'Content-Type': 'application/json', // may Cause Error: Access to fetch at '....' from origin '....' has been blocked by CORS policy: Response to preflight request doesn't pass access control  on https://cors-anywhere.herokuapp.com/'
 
-      'X-Requested-With': 'XMLHttpRequest', // Нужно для CORS Anywhere
-      // 'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(formData),
-  });
-  // console.log('response', response);
+        'X-Requested-With': 'XMLHttpRequest', // Нужно для CORS Anywhere
+        // 'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    console.log('response', response);
 
-  const data = await response.json();
-  // console.log('data', data);
+    //* чисто для бесплатного HTTPS-прокси https://cors-anywhere.herokuapp.com/  выбрасываем и обрабатываем свою ошибку
+    if (response.status === 403) {
+      // alert(
+      //   'Запрос не отправлен. \n\nПодтвердите, что вы человек - перейдите по ссылке "Подтвердить"\n и нажмите кнопку "Request temporary access to the demo server"'
+      // ); // https://cors-anywhere.herokuapp.com/corsdemo
+      // return;
 
-  if (data.status === 'ok') {
-    alert(`${userName}, ваш запрос отправлен!`);
-  } else {
-    alert(`Произошла ошибка! \n\nЗапрос не отправлен.`);
+      throw new Error(
+        'Запрос не отправлен. \n\nПодтвердите, что вы человек - перейдите по ссылке "Подтвердить"\n и нажмите кнопку \n"Request temporary access to the demo server"'
+      );
+    }
+
+    const data = await response.json();
+    console.log('data', data);
+
+    if (data.status === 'ok') {
+      alert(`${userName}, ваш запрос отправлен!`);
+    } else {
+      alert(`Произошла ошибка! \n\nЗапрос не отправлен.`); // ошибка отправки в тг боте
+    }
+
+    tgform.reset();
+    closeModal({ target: closeModalBtn });
+  } catch (error) {
+    console.log(error.message);
+    alert(error.message);
   }
-
-  tgform.reset();
-  closeModal({ target: closeModalBtn });
 }
 
 tgform.addEventListener('submit', formSubmitSafeBackEnd);
@@ -64,3 +82,14 @@ tgform.addEventListener('submit', formSubmitSafeBackEnd);
 // 1. Add form validation
 // 2. Add errors handling (try..catch)
 // 3. Add CAPTCHA
+
+//! Создаём телеграмм бота
+/**
+ * 1. Переходим в BotFather -> start -> вводим /newbot -> получаем сообщение с токеном
+ * 2. Находим своего бота в тг и отправляем ему любое сообщение
+ * 3. Переходим по адресу https://api.telegram.org/botYOUR_TOKEN/getUpdates
+ * 4. В полученном ответе копируем значение из "id" - это chat_id
+ * 5. Запросы будут приходить в чат с ботом (после настройки frontend и backend))
+ *
+ */
+//! Создаём телеграмм бота
